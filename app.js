55f58ad3934b609daa67ce63aa623fe2bb84f500
Youil-Park <parkyouil98@gmail.com>
2025-01-07 15:53:08 +0900

    게임 로직 구현하기

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const ROCK = 'ROCK';
 const PAPER = 'PAPER';
 const SCISSORS = 'SCISSORS';
 const DEFAULT_USER_CHOICE = ROCK;
+const RESULT_DRAW = 'DRAW'; // 비길때
+const RESULT_PLAYER_WINS = 'PLAYER_WINS';     // 사용자가 이길때
+const RESULT_COMPUTER_WINS = 'COMPUTER_WINS'; // 컴퓨터가 이길때
 
 let gameIsRunning = false;    // 게임을 시작하면 또 다른 게임을 못하게
 
@@ -21,6 +24,39 @@ const getPlayerChoice = function() {
     }
     
     return selection;
+};
+
+// 컴퓨터가 무엇을 낼지 선택
+const getComputerChoice = function() {
+  // 0 ~ 0.33 = ROCK
+  // 0.34 ~ 0.67 = PAPER
+  // 0.68 ~ 1 = SCISSORS
+  
+  const ramdomValue = Math.random();
+
+  if(ramdomValue < 0.34) {
+    return ROCK;
+  } else if (ramdomValue < 0.67) {
+    return PAPER;
+  } else {
+    return SCISSORS;
+  }
+};
+
+// 승자가 누구인지 확인
+const getWinner = function(cChoice, pChoice) {
+  // 비길때
+  if(cChoice === pChoice){
+    return RESULT_DRAW;
+  } else if(  // 사용자가 이김
+    cChoice === ROCK && pChoice === PAPER || 
+    cChoice === PAPER && pChoice === SCISSORS ||
+    cChoice === SCISSORS && pChoice === ROCK
+  ){
+    return RESULT_PLAYER_WINS;
+  } else {  // 컴퓨터가 이김
+    return RESULT_COMPUTER_WINS;
+  }
 }
 
 startGameBtn.addEventListener('click', function() {
@@ -29,6 +65,8 @@ startGameBtn.addEventListener('click', function() {
   }
   gameIsRunning = true;
   console.log('Game is starting...');
-  const playersSelection = getPlayerChoice();
-  console.log(playersSelection);
+  const playerChoice = getPlayerChoice();
+  const computerChoice = getComputerChoice();
+  const winner = getWinner(computerChoice, playerChoice);
+  console.log(winner);
 });
